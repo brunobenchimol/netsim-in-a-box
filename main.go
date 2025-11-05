1cf10653558f553d475ca5cdb87a5c8f12f7edc2
brunobenchimol <brunobenchimol@gmail.com>
2025-11-05 16:24:45 -0300

    fix on index.html new ui

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ type PreflightCheck struct {
 var isDarwin bool
 var hasIFB bool
 
-const version = "4.2.0" // V4: Pure Go TC
+const version = "4.2.1" // V4: Pure Go TC
 const apiVersion = "v2" // The API path we are serving
 
 func init() {
